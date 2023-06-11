2790897630d56b417bfa09bd963bd707471f892a
low-batt <86170219+low-batt@users.noreply.github.com>
2023-06-10 22:47:36 -0400

    Update libjxl
    This commit will:
    - Add .DS_Store to .gitignore
    - Lower macOS deployment target to 10.15 from 11.0
    - Update build_libjxl.sh to work with latest libjxl version
    - Update libjxl libraries and include files
    - Remove use of libbrotlienc
    - Change code signing to sign to run locally
    - Disable library validation

--- build_libjxl.sh
+++ build_libjxl.sh
@@ -7,7 +7,7 @@ mkdir -p build
 pushd build
 
 
-CMAKE_OSX_ARCHITECTURES='x86_64;arm64' cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET='11.0' ..
+CMAKE_OSX_ARCHITECTURES='x86_64;arm64' cmake -DJPEGXL_STATIC=true -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET='10.15' ..
 CMAKE_OSX_ARCHITECTURES='x86_64;arm64' cmake --build . --target jxl-static -- -j
 CMAKE_OSX_ARCHITECTURES='x86_64;arm64' cmake --build . --target jxl_threads-static -- -j
 
@@ -18,7 +18,10 @@ mkdir -p jpeg-xl/lib
 mkdir -p jpeg-xl/include/jxl
 cp -R libjxl/build/lib/libjxl*.a jpeg-xl/lib
 cp -R libjxl/build/third_party/highway/libhwy.a jpeg-xl/lib
-cp -R libjxl/build/third_party/brotli/libbrotli*-static.a jpeg-xl/lib
+
+# Only need decoder, avoid copying the encoder library.
+cp -R libjxl/build/third_party/brotli/libbrotlicommon.a jpeg-xl/lib
+cp -R libjxl/build/third_party/brotli/libbrotlidec.a jpeg-xl/lib
 
 cp -R libjxl/build/lib/include/jxl/* jpeg-xl/include/jxl/
 cp -R libjxl/lib/include/jxl/* jpeg-xl/include/jxl/
